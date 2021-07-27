497292178ad9c944f0b05d7c4e843e848b920705
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-07-28 00:43:18 +0300

    add a url pattern for register to rediret to the register form

--- hoodproject/urls.py
+++ hoodproject/urls.py
@@ -20,5 +20,5 @@ from hoodapp import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hoodapp.urls')),
-    path('register/', user_views.register, name = 'register'),
+     path('register/', user_views.register, name = 'register'),
 ]
